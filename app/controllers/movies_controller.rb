369d716a1d26553983fb6e348d41b789887f3ce6
yolayne <cloud.layne@gmail.com>
2013-10-12 18:46:09 -0700

    Working on the local server

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,59 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+
+
+    @all_ratings = Movie.all_ratings
+    redirect = false
+
+    if params[:ratings] == nil
+      if session[:ratings] != nil
+        flash.keep
+        params[:ratings] = session[:ratings]
+        redirect = true
+      end
+    else
+      session[:ratings] = params[:ratings]
+    end
+
+    if params[:sort] == nil
+      if session[:sort] != nil
+        flash.keep
+        params[:sort] = session[:sort]
+        redirect = true
+      end
+    else
+      session[:sort] = params[:sort]
+    end
+
+    if redirect == true
+      redirect_to :sort => session[:sort], :ratings => session[:ratings], :commit => params[:commit]
+    end
+
+    @sort = params[:sort]
+    if params[:ratings] != nil
+      @checked_ratings = params[:ratings] 
+    else
+      @checked_ratings = @all_ratings
+    end
+
+    if @sort.nil?
+      if @checked_ratings.nil?
+        @movies = Movie.all
+      elsif @checked_ratings == @all_ratings
+        @movies = Movie.find(:all, :conditions => {:rating => @checked_ratings})
+      else
+        @movies = Movie.find(:all, :conditions => {:rating => @checked_ratings.keys})
+      end
+    else
+      if @checked_ratings != params[:ratings]
+        @movies = Movie.find(:all, :order => @sort)
+      elsif @checked_ratings == @all_ratings
+        @movies = Movie.find(:all, :order => @sort, :conditions => { :rating => @checked_ratings})
+      else
+        @movies = Movie.find(:all, :order => @sort, :conditions => { :rating => @checked_ratings.keys})
+      end
+    end
   end
 
   def new
